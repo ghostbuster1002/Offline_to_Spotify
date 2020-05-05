8b350ce224787213b81ecc16ef9e109a9b84205a
Kapil Kalra <kalras.kapil@gmail.com>
2020-05-05 16:48:32 +0530

    Read and search modules integrated in test.py

--- Spotify Search.py
+++ Spotify Search.py
@@ -2,14 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-trackname="California Dreamin'"
-artist= "The Mamas & The Papas"
+trackname="You Owe Me"
+artist= "The Chainsmokers"
 album=''
 query=trackname+' artist:'+artist
 results = sp.search(query,type='track')
-try:
-    url=results['tracks']['items'][0]['uri']
-except:
-    print('it throws error')
-else:
-    print('it doesn\'t throw an error',url)
+print(results['tracks']['items'][0]['uri'])
--- test.py
+++ test.py
@@ -1,73 +1,54 @@
-# import urllib.parse as p #For Parsing the Track Name with
-# import spotipy
-# import spotipy.util as util
-# from spotipy.oauth2 import SpotifyClientCredentials
-#
-# client_credentials_manager = SpotifyClientCredentials(client_id='01e4ef84a877453e8b4f711e15ee4951',
-#                                                       client_secret='2faaaf0f2f3d4a0584540d889d735c19')
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-# username='kapilkalra'
-# scope='user-modify-playback-state playlist-modify-private user-read-private'
-# token=util.prompt_for_user_token(username, scope,
-#                            client_id='01e4ef84a877453e8b4f711e15ee4951',
-#                            client_secret='2faaaf0f2f3d4a0584540d889d735c19',
-#                            redirect_uri='http://localhost:80')
-# sp=spotipy.client.Spotify(auth=token)
-# print(token)
-# trackname = 'Get Lucky'
-# artistname= 'Daft Punk'
-# albumname= 'Random Access Memories'
-# query='track:'+trackname+' artist:'+artistname+' album:'+albumname
-# q=p.quote(query,safe=':')
-# print(q)
-# sp.set_auth(token)
-# s_result=sp.search(q,type='track')
-# print(s_result)
-# sp.add_to_queue('spotify:track:0dEIca2nhcxDUV8C5QkPYb', device_id=None).
-# #https://developer.spotify.com/documentation/web-api/reference/search/search/
-# #https://spotipy.readthedocs.io/en/2.12.0/#examples
-# #spotify:playlist:6E1aD3WKDlg0355v4dzbsk (test 1 Playlist)
 import os
 import spotipy
+from tinytag import TinyTag
+sp = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
 
+def query(track):
+    q = track['title'] + ' artist:' + track['artist']
+    if (track['album']):
+        q+= ' album:' + track['album']
+    return q
 
+def urisearch(track):
+    sresult = sp.search(query(track), type='track', limit=1)
+    if sresult['tracks']['items']:
+        track['uri'] = sresult['tracks']['items'][0]['uri']
+        return [track,True]
+    else:
+        return [track,False]
+
+def searchbyfilename(track,filename):
+    name=filename.split(' - ')
+    track['title']=name[0]
+    track['artist']=os.path.splitext(name[1])[0]
+    track['album']=''
+    return urisearch(track)
 
 def gettracks(path):
-    track_list=[]
-    track_info=['title','artist','album']
+    track_info=['title','artist','album','uri']
     track_data=dict.fromkeys(track_info)
+    track_list=[]
+    found=False
     for subdir, dirs, files in os.walk(path):
         for filename in files:
             if filename.endswith((".m4a",".mp3")):
                 tag=TinyTag.get(subdir + os.sep + filename)
                 for x in track_info:
-                    track_data[x]=getattr(tag,x)
+                    if x is 'uri':
+                        while found is False:
+                            [track_data,found]=urisearch(track_data) #Search by ID3Tag
+                            if found is not True:     #Try one more time with filename
+                                [track_data,found]=searchbyfilename(track_data,filename)
+                                if track_data['album']=='' and found is False:
+                                    break
+                                else:
+                                    track_data['album']=''
+                    else:
+                        track_data[x]=getattr(tag,x) #Get ID3 data
                 track_list.append(track_data.copy())
-    return track_lists
-
-def spotifysearch(track_list):
-    sp = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
-    failed_tracks=[]
-    uri_list=[]
-
-    for track in track_list:
-        query=track['title'] + ' artist:' + track['artist']
-        if(track['album']):
-            query+= ' album:' + track['album']
-        sresults = sp.search(query, type='track', limit=1)
-        try:
-            uri=sresults['tracks']['items'][0]['uri']
-        except:
-            failed_tracks.append(track)
-        else:
-            track_dict.append(uri)
-    results = {
-        "successful": uri_list,
-        "failed": failed_tracks
-    }
-    return results
+    return track_list
 
 path=r'F:\New_Music'
 tracks=gettracks(path)
-results=spotifysearch(tracks)
-print('URI found:' )
+for track in tracks:
+    print(track['title'],'-',track['artist'],' (',track['album'],') ',track['uri'])
